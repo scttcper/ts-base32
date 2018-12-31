6788f02afce23f9c672869f50e16899dbdb26485
Scott Cooper <scttcper@gmail.com>
2018-12-30 18:32:46 -0800

    docs: use input event

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -8,8 +8,8 @@ import { base32Decode, base32Encode, hexToArrayBuffer } from '../../src';
 const input = document.querySelector<HTMLInputElement>('#input');
 const output = document.querySelector<HTMLInputElement>('#output');
 
-input.addEventListener('keyup', event => inputChange((event.target as HTMLInputElement).value));
-output.addEventListener('keyup', event => outputChange((event.target as HTMLInputElement).value));
+input.addEventListener('input', event => inputChange((event.target as HTMLInputElement).value));
+output.addEventListener('input', event => outputChange((event.target as HTMLInputElement).value));
 
 function inputChange(str: string) {
   output.value = base32Encode(Buffer.from(str));
