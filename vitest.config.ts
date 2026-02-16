5adbe76d6cc5503b1510323d25ecd787965b78ab
Scott Cooper <scttcper@gmail.com>
2026-02-15 19:02:25 -0800

    remove coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
+    exclude: ['./**/dist/**', './**/node_modules/**'],
   },
 });
